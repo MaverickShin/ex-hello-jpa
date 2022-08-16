a7ee13b2cb0f436afa6994b3ae766320e4d12449
MaverickShin <dobinu1102@gmail.com>
2022-08-17 08:58:18 +0900

    entity mapping

---
+++ src/main/java/hellojpa/UserMain.java
@@ -0,0 +1,40 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class UserMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+
+            Users user = new Users();
+            /*user.setId(2L);
+            user.setUsername("B");
+            user.setRoleType(RoleType.ADMIN);*/
+
+            // @GeneratedValue(strategy = GenerationType.AUTO) 때문에 id값은 자동 생성 됨
+            user.setUsername("C");
+
+            em.persist(user);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
---
+++ src/main/java/hellojpa/Users.java
@@ -0,0 +1,94 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Users {
+
+    // strategy = GenerationType.AUTO : 데이터베이스 방언에 따라 자동 생성
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    // nullable = false : not null 제약조건
+    @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default 'EMPTY'")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp;
+
+    public Users() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
